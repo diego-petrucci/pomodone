0653242079bc262826b89ee0404e32926e7a3561
Diego Petrucci <diegodearaujopetrucci@outlook.com>
2026-02-09 23:26:17 -0300

    adicionando botão

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import './styles/theme.css'
 import './styles/global.css'
 import { Heading } from './components/Heading'
+import { TimerIcon } from 'lucide-react'
 
-export function App(){
-  console.log("oi")
-
+export function App() {
   return (
     <div>
-      <Heading></Heading>
+      <Heading>
+        Olá mundo!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam rerum perspiciatis quos ad minima nobis, consequuntur totam commodi fuga, dolorum explicabo ducimus natus accusantium in, animi repudiandae. Omnis, expedita officiis.</p>
     </div>
   )
