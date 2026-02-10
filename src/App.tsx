ee99d1c45d8ea9a0c83e9e40906a3068dc3ec872
Diego Petrucci <diegodearaujopetrucci@outlook.com>
2026-02-09 21:38:22 -0300

    css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './styles/theme.css'
 import './styles/global.css'
+import { Heading } from './components/Heading'
 
 export function App(){
   console.log("oi")
 
   return (
     <div>
-      <h1>Olá mundo</h1>
+      <Heading></Heading>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam rerum perspiciatis quos ad minima nobis, consequuntur totam commodi fuga, dolorum explicabo ducimus natus accusantium in, animi repudiandae. Omnis, expedita officiis.</p>
     </div>
   )
