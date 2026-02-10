05dc3ff9d2ddc17f683cd5a0ec052aa44920d4c0
Diego Petrucci <diegodearaujopetrucci@outlook.com>
2026-02-10 18:10:47 -0300

    Refatoração Boas práticas

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,12 @@
-import { Container } from './components/Container'
-import { Logo } from './components/Logo'
-
 import './styles/theme.css'
 import './styles/global.css'
-import { Menu } from './components/Menu'
-import { CountDown } from './components/CountDown'
-import { DefaultInput } from './components/DefaultInput'
-import { Cycles } from './components/Cycles'
-import { DefaultButton } from './components/DefaultButton'
-import { PlayCircleIcon } from 'lucide-react'
-import { Footer } from './components/Footer'
+import { Home } from './pages/Home'
 
 
 export function App() {
 
   return (
-    <>
-      <Container>
-        <Logo></Logo>
-      </Container>
-
-      <Container>
-        <Menu></Menu>
-      </Container>
-
-      <Container>
-        <CountDown></CountDown>
-      </Container>
-
-      <Container>
-        <form className='form' action="">
-          <div className='formRow'>
-            <DefaultInput labelText={"task"} id='MeuInput' type='text' placeholder='Digite Algo' />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer></Footer>
-      </Container>
-    </>)
+    <Home />
+  )
 }
 
